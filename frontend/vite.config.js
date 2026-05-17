94eb926e60ead9259d4f2003b8a7196957f71a6e
David,Fuentes Martínez <dfuemar606@g.educaand.es>
2026-05-17 17:09:11 +0200

    CONFIGURACION WEB

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss()
+  ],
   preview: {
     allowedHosts: [
       'webincidencias-production.up.railway.app'
